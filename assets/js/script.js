c6640f0a36930c5f4db53ccd08a4a50377ab7a38
Rob Benney <rob.benney@gmail.com>
2022-03-31 20:02:13 -0400

    fetch request update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-const options = {
+
+//fetch requests to both APIs - ROB
+var options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Host': 'numbersapi.p.rapidapi.com',
@@ -6,12 +8,14 @@ const options = {
 	}
 };
 
+//Random Number fact
 fetch('https://numbersapi.p.rapidapi.com/6/21/date?fragment=true&json=true', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
-
-    fetch('https://cat-fact.herokuapp.com/facts')
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.log(err));
+    
+//Random Cat fact
+fetch('https://cat-fact.herokuapp.com/facts')
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
