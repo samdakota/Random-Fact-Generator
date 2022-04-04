31a8ec5ab1a5818c55ef5d31c2bde2d6bbf6e253
Rob Benney <rob.benney@gmail.com>
2022-04-03 22:34:21 -0400

    working on displaying the fact

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+var categoryContainerEl = document.querySelector('#category-container')
+var factContainerEl = document.querySelector('#fact-container')
+var savedFactContainerEl = document.querySelector('#saved-fact-container')
+var saveFactBtn = document.querySelector('#save-fact')
+var newFactBtn = document.querySelector('#new-fact')
+var savedFactBtn = document.querySelector('#saved-fact')
+
+var getCatFactHandler = function(event) {
+    event.preventDefault();
+
+   getCatFact();
+};
+
+var getNumberFactHandler = function(event) {
+    event.preventDefault();
+
+    getNumberFact();
+};
 
 // //fetch requests to both APIs - ROB
 // var options = {
@@ -30,15 +48,15 @@ var getNumberFact = function(data) {
         // request was successful
         if (response.ok) {
           console.log(response);
-          response.json().then(function(text) {
-            console.log(text);
+          response.json().then(function(data) {
+            console.log(data);
             getNumberFact(data, options);
           });
-        } else {
+        } else { //take this out at the end, just using to make sure it works
           alert("Error: " + response.statusText);
         }
       })
-      .catch(function(error) {
+      .catch(function(error) { //take this out at the end, just using to make sure it works
         alert("Unable to connect to Number API");
       });
   };
@@ -53,15 +71,27 @@ var getNumberFact = function(data) {
         // request was successful
         if (response.ok) {
           console.log(response);
-          response.json().then(function(text) {
-            console.log(text);
-            getCatFact(text);
+          response.json().then(function(data) {
+            console.log(data);
+            getCatFact(data);
           });
-        } else {
+        } else { //take this out at the end, just using to make sure it is working
           alert("Error: " + response.statusText);
         }
       })
-      .catch(function(error) {
+      .catch(function(error) { //take this out at the end, just using to make sure it is working
         alert("Unable to connect to Cat API");
       });
   };
+
+  var displayFact = function() {
+
+  }
+  //button clicked on fact category
+  //call to API that was clicked
+  //parse data to display fact
+  //button for save fact
+  //button listener to save fact to local storage
+    //localStorage.setItem(""); save to local storage
+    //localStorage.getItem(""); get it from local storage
+
