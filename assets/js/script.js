77bab958123c2e44327f403a4abb92d23e444a9f
Rob Benney <rob.benney@gmail.com>
2022-04-07 19:48:32 -0400

    final refactoring of Display functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,12 @@ var categoryContainerEl = document.getElementById('category-container')
 var newFactContainerEl = document.getElementById('fact-container')
 var savedFactContainerEl = document.getElementById('saved-fact-container')
 
-// buttons
+// Buttons
 var saveFactBtn = document.getElementById('save-fact')
 var newFactBtn = document.getElementById('new-fact')
 var savedFactBtn = document.getElementById('saved-fact')
 
-// generate category buttons
+// Generate category buttons
 const categories = [
   {
     name:"Numbers",
@@ -23,37 +23,57 @@ function createCategoryBtns() {
   for (let i = 0; i < categories.length; i++) {
     var categoryButton = document.createElement("button");
     categoryButton.onclick = displayNewCatFact;
-    // categoryButton.onclick = displayNewNumberFact;
     categoryButton.textContent = categories[i].name;
     if (categories[i].name == "Cats") {
       categoryButton.setAttribute("id", "cat-button")
     } else if (categories[i].name == "Numbers") {
-      categoryButton.setAttribute("id", "numbers-button");
+      categoryButton.onclick = displayNewNumberFact;
     }
     categoryContainerEl.appendChild(categoryButton);
   }
 }
 createCategoryBtns();
 
+//Display Cat Fact
 function displayNewCatFact() {
   var newCatFactEl = document.createElement("h1");
-  newCatFactEl.textContent = getCatFact();
-  newFactContainerEl.appendChild(newCatFactEl);
-  categoryContainerEl.setAttribute("class", "hide");
-  newFactBtn.removeAttribute("class", "hide");
-  saveFactBtn.removeAttribute("class", "hide");
+  var catApiUrl = "https://cat-fact.herokuapp.com/facts";
+
+  // Fetch request to url
+  fetch(catApiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var newCatFact = data[randomNumber()].text;
+      newCatFactEl.textContent = newCatFact;
+      newFactContainerEl.appendChild(newCatFactEl);
+      categoryContainerEl.setAttribute("class", "hide");
+      newFactBtn.removeAttribute("class", "hide");
+      saveFactBtn.removeAttribute("class", "hide");
+  });
+}
+//Display Number Fact
+function displayNewNumberFact() {
+  var newNumberFactEl = document.createElement("h1");
+  var numberApiUrl = "https://numbersapi.p.rapidapi.com/50/trivia?fragment=true&notfound=floor&json=true";
+
+  //Fetch request to url
+  fetch(numberApiUrl, options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var newNumberFact = data.text;
+      newNumberFactEl.textContent = "50 is " + newNumberFact;
+      newFactContainerEl.appendChild(newNumberFactEl);
+      categoryContainerEl.setAttribute("class", "hide");
+      newFactBtn.removeAttribute("class", "hide");
+      saveFactBtn.removeAttribute("class", "hide");
+    });
 }
 
-// function displayNewNumberFact() {
-//   var newNumberFactEl = document.createElement("h1");
-//   newNumberFactEl.textContent = getNumberFact();
-//   newNumberFactContainerEl.appendChild(newNumberFactEl);
-//   categoryContainerEl.setAttribute("class", "hide");
-//   newFactBtn.removeAttribute("class", "hide");
-//   saveFactBtn.removeAttribute("class", "hide");
-// }
-
-//fetch requests to both APIs
+//Info for Numbers API
 var options = {
   method: "GET",
   headers: {
@@ -62,54 +82,7 @@ var options = {
   },
 };
 
-// create random number less than max value
+// Create random number for cat fact
 function randomNumber() {
   return Math.floor(Math.random() * 5);
 }
-
-var getNumberFact = function (number) {
-  var numberApiUrl =
-    "https://numbersapi.p.rapidapi.com/"+number+"/trivia?fragment=true&notfound=floor&json=true";
-
-  // make a get request to url
-  fetch(numberApiUrl, options)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log("numberapi = ", data);
-
-      var newNumberFact = data.text;
-      console.log(newNumberFact);
-      return newNumberFact;
-    });
-};
-getNumberFact(50);
-
-var getCatFact = function () {
-  var catApiUrl = "https://cat-fact.herokuapp.com/facts";
-  // var newCat = ""
-
-  // make a get request to url
-  fetch(catApiUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log("catapiurl = ", data);
-
-    var newCatFact = data[randomNumber()].text;
-     console.log(newCatFact);
-    return newCatFact; 
-  });
-  // console.log("newCat = ", newCat)
-  // return newCat;
-};
-//  getCatFact();
-
-
-// localStorage.setItem("fact", "specific fact that was pulled");
-
-
-// localStorage.getItem("fact");
-
