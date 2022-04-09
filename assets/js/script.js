8e69e9c99d62e81e304fb1f276b12f103e0cb111
Sam Miller <sam.dakota@gmail.com>
2022-04-09 13:01:50 -0400

    refactoring functions plus New Fact Button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,82 @@
-var categoryContainerEl = document.getElementById('category-container')
-var newFactContainerEl = document.getElementById('fact-container')
-var savedFactContainerEl = document.getElementById('saved-fact-container')
+var categoryContainerEl = document.getElementById("category-container");
+var newFactContainerEl = document.getElementById("fact-container");
+var savedFactContainerEl = document.getElementById("saved-fact-container");
 
 // Buttons
-var saveFactBtn = document.getElementById('save-fact')
-var newFactBtn = document.getElementById('new-fact')
-var savedFactBtn = document.getElementById('saved-fact')
+var saveFactBtn = document.getElementById("save-fact");
+var newFactBtn = document.getElementById("new-fact");
+var savedFactBtn = document.getElementById("saved-fact");
+newFactBtn.addEventListener("click", newFact);
 
+const categoriesObj = {
+  Numbers: "http://numbersapi.com/random",
+  Cats: "https://cat-fact.herokuapp.com/facts",
+};
 
 // Generate category buttons
 const categories = [
   {
-    name:"Numbers",
-    API:"https://numbersapi.p.rapidapi.com/50/trivia?fragment=true&notfound=floor&json=true",
-
+    name: "Numbers",
+    API: "http://numbersapi.com/random",
   },
   {
-    name:"Cats",
-    API:"https://cat-fact.herokuapp.com/facts",
+    name: "Cats",
+    API: "https://cat-fact.herokuapp.com/facts",
+  },
+];
+
+document.addEventListener("click", function (event) {
+  if (event.target.className === "fact-button") {
+    var id = event.target.id;
+    displayNewFact(categoriesObj[id], id);
   }
-]
+});
 
 function createCategoryBtns() {
-  for (let i = 0; i < categories.length; i++) {
+  for (var key in categoriesObj) {
     var categoryButton = document.createElement("button");
-    categoryButton.onclick = displayNewCatFact;
-    categoryButton.textContent = categories[i].name;
-    if (categories[i].name == "Cats") {
-      categoryButton.setAttribute("id", "cat-button")
-    } else if (categories[i].name == "Numbers") {
-      categoryButton.onclick = displayNewNumberFact;
-    }
+    categoryButton.setAttribute("class", "fact-button");
+    categoryButton.setAttribute("id", key);
+    categoryButton.textContent = key;
     categoryContainerEl.appendChild(categoryButton);
   }
 }
 createCategoryBtns();
 
-//Display Cat Fact
-function displayNewCatFact() {
-  var newCatFactEl = document.createElement("h1");
-  var catApiUrl = "https://cat-fact.herokuapp.com/facts";
-
-  // Fetch request to url
-  fetch(catApiUrl)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var newCatFact = data[randomNumber()].text;
-      newCatFactEl.textContent = newCatFact;
-      newFactContainerEl.appendChild(newCatFactEl);
-      categoryContainerEl.setAttribute("class", "hide");
-      newFactBtn.removeAttribute("class", "hide");
-      saveFactBtn.removeAttribute("class", "hide");
-  });
+function newFact() {
+  console.log("test");
+  categoryContainerEl.removeAttribute("class", "hide");
+  newFactBtn.setAttribute("class", "hide");
+  saveFactBtn.setAttribute("class", "hide");
+  newFactContainerEl.setAttribute("class", "hide");
 }
-//Display Number Fact
-function displayNewNumberFact() {
-  var newNumberFactEl = document.createElement("h1");
-  var numberApiUrl = "https://numbersapi.p.rapidapi.com/50/trivia?fragment=true&notfound=floor&json=true";
 
-  //Fetch request to url
-  fetch(numberApiUrl, options)
+function displayNewFact(api_url, kind) {
+  var newFactEl = document.createElement("h1");
+  fetch(api_url)
     .then(function (response) {
-      return response.json();
+      if (kind === "Cats") {
+        return response.json();
+      } else {
+        return response.text();
+      }
     })
     .then(function (data) {
-      var newNumberFact = data.text;
-      newNumberFactEl.textContent = "50 is " + newNumberFact;
-      newFactContainerEl.appendChild(newNumberFactEl);
+      if (kind === "Cats") {
+        var newCatFact = data[randomNumber()].text;
+        newFactEl.textContent = newCatFact;
+      } else {
+        var newNumberFact = data;
+        newFactEl.textContent = newNumberFact;
+      }
+      newFactContainerEl.innerHTML = "";
+      newFactContainerEl.appendChild(newFactEl);
+      newFactContainerEl.removeAttribute("class", "hide");
       categoryContainerEl.setAttribute("class", "hide");
       newFactBtn.removeAttribute("class", "hide");
       saveFactBtn.removeAttribute("class", "hide");
     });
 }
-
 //Info for Numbers API
 var options = {
   method: "GET",
@@ -88,3 +90,15 @@ var options = {
 function randomNumber() {
   return Math.floor(Math.random() * 5);
 }
+
+// function createSavedFactEl() {
+//   var savedFact = document.createElement("li");
+//   savedFact.textContent =
+// }
+
+// function displaySavedFacts() {
+//   var savedFacts = localStorage.getItem('facts');
+//   for (let i = 0; i < savedFacts.length; i++) {
+//     createSavedFactEl(savedFacts[i]);
+//   }
+// }
